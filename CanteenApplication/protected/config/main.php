e3ee303303d8e004222d257a1e2d4494b20c7ab6
Kailesh Ramjee <kaileshramjee@gmail.com>
2012-09-12 12:40:47 +0200

    First commit: Created Andorid and Canteen projects

---
+++ CanteenApplication/protected/config/main.php
@@ -0,0 +1,86 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'RMB Corporate Canteen Management System',
+
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	// autoloading model and component classes
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+	),
+		
+	'modules'=>array(
+		'gii'=>array(
+			'class'=>'system.gii.GiiModule',
+			'password'=>'zaq123',
+		),
+		),
+		
+
+	'defaultController'=>'post',
+
+	// application components
+	'components'=>array(
+		'user'=>array(
+			// enable cookie-based authentication
+			'allowAutoLogin'=>true,
+		),
+		/*	
+		'db'=>array(
+			'connectionString' => 'sqlite:/var/www/yii/protected/data/blog.db',
+			'tablePrefix' => 'tbl_',
+		),
+		*/
+		// uncomment the following to use a MySQL database
+		
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=blog',
+			'emulatePrepare' => true,
+			'username' => 'root',
+			'password' => 'zaq123',
+			'charset' => 'utf8',
+			'tablePrefix' => 'tbl_',
+		),
+		
+		'errorHandler'=>array(
+			// use 'site/error' action to display errors
+            'errorAction'=>'site/error',
+        ),
+        'urlManager'=>array(
+        	'urlFormat'=>'path',
+        	'rules'=>array(
+        		'post/<id:\d+>/<title:.*?>'=>'post/view',
+        		'posts/<tag:.*?>'=>'post/index',
+        		'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+        	),
+        ),
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+				// uncomment the following to show log messages on web pages
+				/*
+				array(
+					'class'=>'CWebLogRoute',
+				),
+				*/
+			),
+		),
+	),
+
+	// application-level parameters that can be accessed
+	// using Yii::app()->params['paramName']
+	'params'=>require(dirname(__FILE__).'/params.php'),
+);
