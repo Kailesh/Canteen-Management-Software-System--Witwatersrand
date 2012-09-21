46774805566acfa84a4563525d5409362622ca2a
Patrick Chen <patrickc3832@gmail.com>
2012-09-21 11:18:42 +0200

    Update mobileTest.php that is used to test an HTTP request from the Android phone and a HTTP response from the web server

--- CanteenApplication/protected/views/site/mobileTest.php
+++ CanteenApplication/protected/views/site/mobileTest.php
@@ -1,5 +1,38 @@
 <?php 
 
-echo 'Hello there you are now connected to the canteen web server';
+require_once 'HTTP/Request2/Response.php';
+
+//echo "Hello there you are now connected to the canteen web server <br />";
+
+/*HttpResponse::status(200);
+HttpResponse::setContentType('json');
+//HttpResponse::setHeader('From', 'Lymber');
+HttpResponse::setData($_POST);
+HttpResponse::send();*/
+
+$connection = Yii::app()->db;
+
+$sql_select = 'SELECT *   FROM menu';
+
+$command = $connection->createCommand($sql_select);
+
+$menuData = $command->query();
+
+$row = $menuData->readAll();
+
+$json_string = json_encode(array("updated" => "false", 'menu:'=>$row));
+
+$headers = http_get_request_headers();
+$result = http_get_request_body();
+
+$decodeResult = json_decode($result);
+
+HttpResponse::status(200);
+HttpResponse::setContentType('application/json');
+//HttpResponse::setHeader('From', 'Lymber');
+HttpResponse::setData($json_string);
+HttpResponse::send();
+
+flush();
 
 ?>
