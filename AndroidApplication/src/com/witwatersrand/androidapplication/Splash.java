179968d1dd2849238356f0eae39ed8dbd1efca65
Kailesh Ramjee <kaileshramjee@gmail.com>
2012-09-24 20:42:02 +0200

    Created a splash screen (Learning how activities work)

---
+++ AndroidApplication/src/com/witwatersrand/androidapplication/Splash.java
@@ -0,0 +1,57 @@
+/*
+ * 
+ */
+package com.witwatersrand.androidapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * @author Kailesh
+ * 
+ */
+public class Splash extends Activity {
+
+	/**
+	 * 
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		Thread timerThread = new Thread() {
+
+			/*
+			 * (non-Javadoc)
+			 * 
+			 * @see java.lang.Thread#run()
+			 */
+			@Override
+			public void run() {
+				try {
+					sleep(2000); // 2000 ms = 2 s
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					Intent startCanteenApplication = new Intent("com.witwatersrand.androidapplication.MAIN");
+					startActivity(startCanteenApplication);
+				}
+			}
+		};
+		timerThread.start();
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onPause()
+	 */
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+	
+	
+}
