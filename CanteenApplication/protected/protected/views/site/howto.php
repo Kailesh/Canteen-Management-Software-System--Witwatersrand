5067671dd19bc8a48ba7a6dbd153c5a116272f80
Patrick Chen <patrickc3832@gmail.com>
2012-10-08 16:38:06 +0200

    Massive drag and drop of existing files
    1)Implemented more web service functionality to respond to the android
    phone requests:
    	i- added authentication
    	ii- implemented video fedd
    	iii- query progress manually
    2) added the live_feed image
    3) added logo to the site

---
+++ CanteenApplication/protected/protected/views/site/howto.php
@@ -0,0 +1,34 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - howTo';
+$this->breadcrumbs=array(
+	'How-to',
+);
+?>
+
+<h1> How - to and Information </h1>
+<hr/>
+
+<h2> Main Functionality</h2>
+
+<p> After being logged in you will notice the following tabs, How-to, Incoming Orders and Completed Orders. By default 
+after login you will redirected to the Incoming orders tab.</p>
+
+<h2> Incoming Orders</h2>
+
+<p> In the incoming orders you shall see a table of incoming orders with the following headings (DeviceID, Item, Quantity, Time
+Placed, Delivery Location and Status). The incoming orders are arranged by the newest order first. Canteen staff must update 
+the status of the order by selecting one of the radio buttons beside it. </p>
+
+<h2> Completed Orders</h2>
+
+<p> In the completed orders you shall see a list of all completed orders that are placed that day.</p>
+
+<h2> How - to</h2>
+
+<p> Below are some quick how-to on how to use this application: <br /></p>
+
+<ul>
+<li>The incoming orders will be refreshed automatically every minute, so all that is required is to select the status of the 
+order. <br/> </li>
+<li>To view the completed order, simply click on the "Completed Orders" tab.</li>
+</ul> 
