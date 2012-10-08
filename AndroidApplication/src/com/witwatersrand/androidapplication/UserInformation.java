9a39d27a8c68333aee60b4cbf7cda26d01d4d8c9
Kailesh Ramjee <kaileshramjee@gmail.com>
2012-10-08 19:49:45 +0200

    The application can now exit when the exit button is clicked in the star menu options

--- AndroidApplication/src/com/witwatersrand/androidapplication/UserInformation.java
+++ AndroidApplication/src/com/witwatersrand/androidapplication/UserInformation.java
@@ -5,10 +5,11 @@ import android.preference.PreferenceActivity;
 
 public class UserInformation extends PreferenceActivity {
 
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.user_preferences);
+        
     }
-    
 }
